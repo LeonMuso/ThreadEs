f13676f33b9b0d302aaf59ab63176223f8e3fc1d
leomuso <Pinosenx@gmail.com>
2026-04-21 21:58:26 +0200

    yippie

---
+++ ThreadEs3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ThreadEs3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(ProcessoBreve);
+            var t2 = new Thread(ProcessoInfinito);
+            t2.IsBackground = true;
+            t1.Start(); t2.Start();
+            t1.Join(); t2.Join();
+        }
+
+        static void ProcessoInfinito()
+        {
+            while (true)
+            {
+                Console.WriteLine(Random.Shared.Next(0, 11));
+                Thread.Sleep(300);
+            }
+        }
+        static void ProcessoBreve()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
