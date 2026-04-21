afc60391585dd7103f6213c5307f14713ba06961
leomuso <Pinosenx@gmail.com>
2026-04-21 21:46:09 +0200

    Add project files.

---
+++ ThreadEs/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThreadEs
+{
+    internal class Program
+    {
+        static int contatore = 0;
+        static object lockContatore = new object();
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(IncrementaValore);
+            var t2 = new Thread(IncrementaValore);
+            t1.Start(); t2.Start();
+            t1.Join(); t2.Join();
+            Console.WriteLine(contatore);
+        }
+
+        static void IncrementaValore()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (lockContatore)
+                {
+                    contatore++;
+                }
+            }
+        }
+    }
+}
---
+++ ThreadEs2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThreadEs2
+{
+    internal class Program
+    {
+        static int contatore = 0;
+        static SemaphoreSlim lockContatore = new SemaphoreSlim(1,1);
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(IncrementaValore);
+            var t2 = new Thread(IncrementaValore);
+            t1.Start(); t2.Start();
+            t1.Join(); t2.Join();
+            Console.WriteLine(contatore);
+        }
+
+        static void IncrementaValore()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lockContatore.Wait();
+                contatore++;
+                lockContatore.Release();
+            }
+
+        }
+    }
+}
